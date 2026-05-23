bfd42b616be51d2cd1f2ea8a97aa05697092df3d
MariaJulietaMolina <mariajulietamolina91@gmail.com>
2026-05-23 15:44:03 -0300

    creacion de endpoints

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const BASE_URL = 'https://fakestoreapi.com/';
+
+const [method, target, ...args] = process.argv.slice(2);
+
+//funcion principal
+
+async function run() {
+}
+if (!method || !target ) {
+    console.error('Faltan Argumentos');
+    return;
+}
+
+//endoints
+
+if (method === 'GET' && target === 'products') {
+    await getAllProducts();
+}
+else if (method === 'GET' && target.startsWith('products/')) {
+    const productId = target.split('/')[1];
+    await getProductsById(productId);
+}
+else if (method === 'POST' && target === 'products') {
+    await createProduct(args);
+}
+else if (method === 'DELETE' && target.startsWith('products/')) {
+    const productId = target.split('/')[1];
+    await deñeteProduct(productId);
+}
+else {
+    console.log('Comando incorrecto.')
+}
+
 
